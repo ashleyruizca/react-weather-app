cd43e83c3f4522773ae41311b7af8cc9a54e15e6
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-06-27 19:17:45 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Weather app</h1>
-      <footer>
-        This project is coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/ashley-ruiz-carrizales-360257159/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Ashley Ruiz-Carrizales
-        </a>{" "}
-        and is
-        <a
-          href="https://github.com/ashleyruizca/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather app</h1>
+        <footer>
+          This project is coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ashley-ruiz-carrizales-360257159/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Ashley Ruiz-Carrizales
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/ashleyruizca/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
