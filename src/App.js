39cfc754eef73dc056a8c9cbdeb81398b3a0660e
ashleyruizca <156567181+ashleyruizca@users.noreply.github.com>
2024-06-27 13:59:00 -0400

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/ashley-ruiz-carrizales-360257159/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ashley Ruiz-Carrizales
+        </a>{" "}
+        and is
+        <a
+          href="https://github.com/ashleyruizca/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
